94af640a4bfd4b659c5705cfd3da15189e25f9f6
Don Kjer <don.kjer@gmail.com>
2021-01-20 11:33:28 -0800

    Fixing OS_ICON

--- p10k.zsh
+++ p10k.zsh
@@ -188,7 +188,7 @@
   #typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
   #typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
-  # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION="$OS_ICON"
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
